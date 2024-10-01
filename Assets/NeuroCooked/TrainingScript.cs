dafa8126a4a7a021d74241ad3df05043d9381c7f
LommyTea <tlupe3507@gmail.com>
2024-09-30 22:26:13 -0400

    Updating NeuroCooked

--- Assets/NeuroCooked/TrainingScript.cs
+++ Assets/NeuroCooked/TrainingScript.cs
@@ -2,47 +2,52 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.SceneManagement; // For scene management
+using UnityEngine.SceneManagement;
+using UnityEditor.PackageManager;
+using Google.Protobuf.WellKnownTypes;
+using Cysharp.Net.Http;
+using Grpc.Net.Client;
+using Grpc.Core;
+using System;
 
-public class ButtonControlledMSequenceFlasher : MonoBehaviour
+
+
+
+public class Game_Behavior : MonoBehaviour
 {
     public Button startTrainingButton;               // The button to start flashing
     public Button startGameButton;         // The button to switch scenes after flashing
-    public Button failTrainingButton;
     public GameObject flashingCube;          // The object that flashes (should be a GameObject)
-    public float flashInterval = 0.5f;       // Time between flashes
-    public float waitBetweenSequences = 2f;  // Time to wait between sequences
+    private float flashInterval = 0.5f;       // Time between flashes
+    private float waitBetweenSequences = 2f;  // Time to wait between sequences
     public int numberOfTrainingEpochs = 1;   // Number of training epochs
     public TMP_Text instructionsText;         // Text to hide when button is clicked
-    public TMP_Text trainingText;            //Text to show when all epochs are done
-    public TMP_Text failureText;
-
-
-
-    private int[][] mSequences = new int[3][] {
-        new int[] { 1, 0, 1, 0, 1 },  // Sequence 1
-        new int[] { 0, 1, 0, 1, 0 },  // Sequence 2
-        new int[] { 1, 1, 0, 0, 1 }   // Sequence 3
-    };
-
+    public rpcClient rpcClient;
+    public int[][] mSequences;
     private bool isFlashing = false;
     private Renderer cubeRenderer;           // Reference to the Renderer of the flashing cube
+    public TMP_Text trainingText;
+    public TMP_Text readyToStart;
+
 
     void Start()
     {
+        //Setting all of the variables/objects based on name in Unity
+        GameObject.Find("Begin_Game").SetActive(false);
+        GameObject.Find("Training_Cube").SetActive(false);
+        rpcClient = FindObjectOfType<rpcClient>();
+        trainingText.gameObject.SetActive(false);
+        readyToStart.gameObject.SetActive(false);
         // Set up button to trigger flashing and hide elements when clicked
         startTrainingButton.onClick.AddListener(OnStartButtonClick);
 
         // Get the Renderer component from the flashing cube
         cubeRenderer = flashingCube.GetComponent<Renderer>();
-        // Ensure the flashing cube is initially inactive
-        flashingCube.SetActive(false);
-        // Ensure the scene switch button is initially inactive
-        startGameButton.gameObject.SetActive(false);
-        failTrainingButton.gameObject.SetActive(false);
-        trainingText.gameObject.SetActive(false);
-        failureText.gameObject.SetActive(false);
+        Debug.Log(rpcClient.Msequences);
+        mSequences = rpcClient.Instance.Msequences;
+        
     }
+
 
     void OnStartButtonClick()
     {
@@ -51,19 +56,22 @@ public class ButtonControlledMSequenceFlasher : MonoBehaviour
             // Hide the UI elements
             instructionsText.gameObject.SetActive(false);
             startGameButton.gameObject.SetActive(false);
+            startTrainingButton.gameObject.SetActive(false);
 
             // Spawn and activate the cube
             flashingCube.SetActive(true);
 
             // Start the flashing process
             StartCoroutine(FlashTrainingSequences());
+            StartCoroutine(waitingForTrainingStatus());
+
+
         }
     }
-
+    
     IEnumerator FlashTrainingSequences()
     {
         isFlashing = true;
-
         for (int epoch = 0; epoch < numberOfTrainingEpochs; epoch++)
         {
             for (int sequenceIndex = 0; sequenceIndex < mSequences.Length; sequenceIndex++)
@@ -79,11 +87,11 @@ public class ButtonControlledMSequenceFlasher : MonoBehaviour
         isFlashing = false;
         // Keep the cube visible
         // Show the switch scene button after flashing is done
+        //Call RPC that calls for training and removes flashing cube
+        flashingCube.SetActive(false); 
         trainingText.gameObject.SetActive(true);
-        //Call RPC that calls for training
-        training();
-        
 
+        rpcClient.Instance.trainingModel();
     }
 
     IEnumerator FlashSequence(int[] sequence, int sequenceNumber)
@@ -103,30 +111,21 @@ public class ButtonControlledMSequenceFlasher : MonoBehaviour
         float duration = endTime - startTime;  // Calculate how long the sequence lasted
 
         // Call the RPC command to send the sequence number and duration (optional)
-        AddSequenceData(sequenceNumber, duration);
+        rpcClient.AddSequenceData(sequenceNumber, duration);
+        Debug.Log("Adding m-sequence" + sequenceNumber);
 
     }
-
-    void AddSequenceData(int sequenceNumber, float duration)
+    IEnumerator waitingForTrainingStatus()
     {
-        // Placeholder for RPC command
-        Debug.Log($"RPC: Sequence {sequenceNumber} finished. Duration: {duration} seconds.");
+        while (!rpcClient.training_status)
+        {
+            yield return null; // Wait until the next frame and check again
+        }
+        trainingText.gameObject.SetActive(false);
+        readyToStart.gameObject.SetActive(true);
+        startGameButton.gameObject.SetActive(true);
 
-        // Here, you would replace this with your actual RPC command to notify the other program
+
     }
-    
-    void training()
-    {
-        int response = await; //training function
-        if (response == 1)
-        {
-            startGameButton.gameObject.SetActive(true);
-        }
-        else
-        {
-            failTrainingButton.gameObject.SetActive(true);
-            failureText.gameObject.SetActive(true);
-        }
- 
-    }
+
 }

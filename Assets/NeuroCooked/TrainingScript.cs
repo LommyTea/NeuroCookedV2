870181095c46e2bf18e709f6edb354c993639a54
LommyTea <tlupe3507@gmail.com>
2024-10-15 17:25:39 -0400

    fixing training scene

--- Assets/NeuroCooked/TrainingScript.cs
+++ Assets/NeuroCooked/TrainingScript.cs
@@ -15,17 +15,28 @@ using System;
 
 public class Game_Behavior : MonoBehaviour
 {
-    public Button startTrainingButton;               // The button to start flashing
-    public Button startGameButton;         // The button to switch scenes after flashing
-    public GameObject flashingCube;          // The object that flashes (should be a GameObject)
+    
+
     private float flashInterval = 0.033f;       // Time between flashes
     private float waitBetweenSequences = 2f;  // Time to wait between sequences
     public int numberOfTrainingEpochs = 1;   // Number of training epochs
-    public TMP_Text instructionsText;         // Text to hide when button is clicked
-    public rpcClient rpcClient;
     public int[][] mSequences;
     private bool isFlashing = false;
+
+    public Button startTrainingButton;               // The button to start flashing
+    public GameObject flashingCube;          // The object that flashes (should be a GameObject)
     private Renderer cubeRenderer;           // Reference to the Renderer of the flashing cube
+
+    public TMP_Text instructionsText;         // Text to hide when button is clicked
+    public TMP_Text trainingModelText;
+    public TMP_Text trainingCompleteText;
+    public rpcClient rpcClient;
+
+    public Button startGameButton;         // The button to switch scenes after flashing
+
+
+
+
 
 
     void Start()
@@ -36,7 +47,12 @@ public class Game_Behavior : MonoBehaviour
         rpcClient = FindObjectOfType<rpcClient>();
         // Set up button to trigger flashing and hide elements when clicked
         startTrainingButton.onClick.AddListener(OnStartButtonClick);
+        instructionsText.gameObject.SetActive(true);
+        startTrainingButton.gameObject.SetActive(true);
 
+        startGameButton.gameObject.SetActive(false);
+        trainingModelText.gameObject.SetActive(false);
+        trainingCompleteText.gameObject.SetActive(false);
         // Get the Renderer component from the flashing cube
         cubeRenderer = flashingCube.GetComponent<Renderer>();
         Debug.Log(rpcClient.Msequences);
@@ -51,45 +67,17 @@ public class Game_Behavior : MonoBehaviour
         {
             // Hide the UI elements
             instructionsText.gameObject.SetActive(false);
-            startGameButton.gameObject.SetActive(false);
             startTrainingButton.gameObject.SetActive(false);
-
             // Spawn and activate the cube
             flashingCube.SetActive(true);
 
             // Start the flashing process
             StartCoroutine(FlashTrainingSequences());
-            //StartCoroutine(waitingForTrainingStatus());
+            StartCoroutine(waitingForTrainingStatus());
 
 
         }
     }
-    
-    IEnumerator FlashTrainingSequences()
-    {
-        isFlashing = true;
-        for (int epoch = 0; epoch < numberOfTrainingEpochs; epoch++)
-        {
-            for (int sequenceIndex = 0; sequenceIndex < mSequences.Length; sequenceIndex++)
-            {
-                // Flash the current sequence once
-                yield return StartCoroutine(FlashSequence(mSequences[sequenceIndex], sequenceIndex + 1));
-
-                // Wait between sequences
-                yield return new WaitForSeconds(waitBetweenSequences);
-            }
-        }
-
-        isFlashing = false;
-        // Keep the cube visible
-        // Show the switch scene button after flashing is done
-        //Call RPC that calls for training and removes flashing cube
-        flashingCube.SetActive(false); 
-        //trainingText.gameObject.SetActive(true);
-
-        rpcClient.Instance.trainingModel();
-    }
-
     IEnumerator FlashSequence(int[] sequence, int sequenceNumber)
     {
         float startTime = Time.time;  // Record start time for duration
@@ -111,17 +99,42 @@ public class Game_Behavior : MonoBehaviour
         Debug.Log("Adding m-sequence" + sequenceNumber);
 
     }
-    //IEnumerator waitingForTrainingStatus()
-    //{
-    //    while (!rpcClient.training_status)
-    //    {
-    //        yield return null; // Wait until the next frame and check again
-    //    }
-    //    trainingText.gameObject.SetActive(false);
-    //    readyToStart.gameObject.SetActive(true);
-    //    startGameButton.gameObject.SetActive(true);
+
+    IEnumerator FlashTrainingSequences()
+    {
+        isFlashing = true;
+        for (int epoch = 0; epoch < numberOfTrainingEpochs; epoch++)
+        {
+            for (int sequenceIndex = 0; sequenceIndex < mSequences.Length; sequenceIndex++)
+            {
+                // Flash the current sequence once
+                yield return StartCoroutine(FlashSequence(mSequences[sequenceIndex], sequenceIndex + 1));
+
+                // Wait between sequences
+                yield return new WaitForSeconds(waitBetweenSequences);
+            }
+        }
+
+        isFlashing = false;
+        // Keep the cube visible
+        // Show the switch scene button after flashing is done
+        //Call RPC that calls for training and removes flashing cube
+        flashingCube.SetActive(false); 
+        trainingModelText.gameObject.SetActive(true);
+        rpcClient.Instance.trainingModel();
+    }
+
+    IEnumerator waitingForTrainingStatus()
+    {
+        while (!rpcClient.training_status)
+        {
+            yield return null; // Wait until the next frame and check again
+        }
+        trainingModelText.gameObject.SetActive(false);
+        trainingCompleteText.gameObject.SetActive(true);
+        startGameButton.gameObject.SetActive(true);
 
 
-    //}
+    }
 
 }
